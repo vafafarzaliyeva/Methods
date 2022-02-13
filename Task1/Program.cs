b0fefa3fdc2f6fab1db05d57103a51745207b619
vafafarzaliyeva <vafahf@code.edu.az>
2022-02-14 01:20:57 +0300

    First Commit

---
+++ Task1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        #region Task1
+        //static void Main(string[] args)
+        //{
+
+
+        //    //Console.WriteLine("String daxil edin");
+        //    //string str = Console.ReadLine();
+
+        //    //Console.WriteLine("Char daxil edin");
+        //    //string simvol = Console.ReadLine();
+        //    //char ch = char.Parse(simvol);
+
+        //    //Console.WriteLine(FindCharIndex(str, ch));
+
+
+
+
+        //}  
+
+
+        //static int FindCharIndex(string str , char ch)
+        //{
+
+        //    for (int i = 0 ; i <str.Length ; i++) {  
+
+
+        //        if (str[i] == ch)
+        //        {
+        //            return i;
+        //        }
+        //    }
+
+        //    return -1;
+        //}
+        #endregion
+        #region Task2
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Eded daxil edin: ");
+        //    string eded = Console.ReadLine();
+        //    int num = int.Parse(eded);
+        //    Console.WriteLine(IsPrime(num));
+        //}
+        //static bool IsPrime(int a)
+        //{
+        //    int say = 0;
+        //    for (int i = 1; i <= a; i++)
+        //    {
+
+        //        if (a % i == 0)
+        //        {
+        //            say++;
+        //        }
+
+
+        //    }
+        //    if (say == 2)
+        //        return true;
+        //    return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //}
+        #endregion
+        #region Task3
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Fenn sayini daxil edin");
+        //    string fennsay = Console.ReadLine();
+        //    int say = int.Parse(fennsay);
+
+        //    int[] arr = new int[say];
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        string k = Console.ReadLine();
+        //        int qiymet = int.Parse(k);
+        //        arr[i] = qiymet;
+
+        //    }
+
+        //    if (CalcAvg(arr, say) > 60)
+        //    {
+        //        Console.WriteLine("Mezun oldunuz");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Mezun ola bilmediniz");
+        //    }
+
+
+
+        //}
+        //static int CalcAvg(int[] arr, int say)
+        //{
+        //    int cem = 0;
+        //    int ortalama = 0;
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        cem += arr[i];
+        //    }
+        //    ortalama = cem / arr.Length;
+
+        //    return ortalama;
+
+        //}
+        #endregion
+    }
+}
+ 
